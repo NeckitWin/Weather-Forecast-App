c2cd464b13ebf0b0f72501f35cdc8c5034469046
NeckitWin <neckit.v@gmail.com>
2024-03-03 21:26:43 +0100

    Add gh-pages

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,8 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
+  publicPath: process.env.NODE_ENV === "production" ? "/Weather-Forecast-App/" : "/",
   plugins: [
     vue(),
   ],
